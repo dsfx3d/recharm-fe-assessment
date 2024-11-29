9e11d180f09b4ed3f123e8964d145726cba80518
dsfx3d <dsfx3d@gmail.com>
2024-11-29 08:04:16 +0530

    fix(useAutoScroll): enhance mutation observer to trigger scroll on input node addition

--- src/hooks/useAutoScroll.tsx
+++ src/hooks/useAutoScroll.tsx
@@ -4,8 +4,14 @@ import {useEffect} from "react";
 export function useAutoScroll(element?: HTMLElement | null) {
   useEffect(() => {
     const observer = new MutationObserver(mutations => {
-      const isNodeAdded = mutations.some(record => record.addedNodes.length);
-      if (isNodeAdded) {
+      const isInputAdded = mutations
+        .flatMap(record => record.addedNodes)
+        // eslint-disable-next-line unicorn/prefer-spread
+        .flatMap(node => Array.from(node))
+        .filter<HTMLElement>(node => node instanceof HTMLElement)
+        .some(node => node.querySelector("input"));
+
+      if (isInputAdded) {
         // defer the scroll to the next frame
         setTimeout(() => scrollToBottom(element), 50);
       }
