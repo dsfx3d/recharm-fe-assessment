1b319b7c22e7b0df81e82f3a975aea4fa8648766
dsfx3d <dsfx3d@gmail.com>
2024-11-29 07:08:31 +0530

    feat(SolutionSchema): change schema to support an array of solutions

--- src/lib/schemas/SolutionSchema.ts
+++ src/lib/schemas/SolutionSchema.ts
@@ -1,8 +1,10 @@
 import {z} from "zod";
 
-export const SolutionSchema = z.object({
-  url: z.string().url(),
-  value: z.string(),
-});
+export const SolutionSchema = z.array(
+  z.object({
+    url: z.string().url(),
+    value: z.string(),
+  }),
+);
 
 export type SolutionSchema = z.infer<typeof SolutionSchema>;
