13f5db45f9e519a6bf4f757c08b2c83d398bac0a
dsfx3d <dsfx3d@gmail.com>
2024-11-30 11:07:11 +0530

    feat(Document): add favicon link to the document head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import {Head, Html, Main, NextScript} from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="https://www.recharm.com/recharm_favicon.ico" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
