0558ace6ba1ad200e5b722a543b1c9ab9895cc9e
dsfx3d <dsfx3d@gmail.com>
2024-11-30 11:07:20 +0530

    feat(Home): add title to the document head for the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,12 @@
 import {CreateRequestMainComponent} from "@/components/CreateRequestMainComponent";
+import Head from "next/head";
 export default function Home() {
-  return <CreateRequestMainComponent />;
+  return (
+    <>
+      <Head>
+        <title>Create New Request</title>
+      </Head>
+      <CreateRequestMainComponent />;
+    </>
+  );
 }
