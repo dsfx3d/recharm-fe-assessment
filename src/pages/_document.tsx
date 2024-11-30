78901e7008ffbf93b234221cfc3d5dbd1a363b19
dsfx3d <dsfx3d@gmail.com>
2024-11-30 16:39:25 +0530

    fix(Document): add theme-color meta tag for improved mobile experience

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="theme-color" content="#7e22ce" />
         <link rel="icon" href="https://www.recharm.com/recharm_favicon.ico" />
       </Head>
       <body className="antialiased">
